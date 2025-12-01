0575a3e12a651befd119206f5c0a774f46a29470
Nero Hoya <dltposiedon@gmail.com>
2025-12-01 22:00:26 +0800

    updated navbar

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
     return (
-        <div class="w-24 h-24 bg-blue-500 hover:w-32 hover:h-32 transition-all duration-300">
+        <div className="w-24 h-24 bg-blue-500 hover:w-32 hover:h-32 transition-all duration-300">
             Hover over me
         </div>)
 }
--- app/navbar.js
+++ app/navbar.js
@@ -1,29 +1,36 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
-    return (
-        // <header>
-        //     <Link href="/" className="navfontChange">Francis Vince Jaca Portfolio</Link>
-        //     <ul>
-        //         <li><Link href="/about" className="navfontChange">About</Link></li>
-        //     </ul>
-        // </header>
+    const pathname = usePathname();
+    const inactiveClasses = "text-white hover:text-gray-300 hover:border-b-4 hover:border-gray-300 navBarText";
+    const activeClasses = "navBarColorActive";
 
-        <nav className="navBackGround p-4 text-white">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/" className="hover:text-gray-300 text-xl font-bold">
-                    Home
-                </Link>
-                <div>
-                    <Link href="/about" className="mr-4 hover:text-gray-300 text-xl font-bold">
-                        About
-                    </Link>
-                    <Link href="/contact" className="hover:text-gray-300 text-xl font-bold">
-                        Contact
-                    </Link>
+    const getLinkClasses = (href) => {
+        const isActive = pathname === href;
+        return ` ${isActive ? activeClasses : inactiveClasses}`;
+    }
+
+
+    return (
+
+        <nav className="fixed top-4 w-screen justify-center items-center px-4 navbar-item">
+            <div className="flex justify-between items-center w-full max-w-6xl mx-auto  p-3 shadow-lg rounded-lg navBarColor">
+                <div className="text-white font-bold" />
+
+                <div className="flex space-x-4">
+
+                    <Link href="/" className={getLinkClasses("/")}>Home</Link>
+                    <Link href="/about" className={getLinkClasses("/about")}>About</Link>
+                    <Link href="/contact" className={getLinkClasses("/contact")}>Contact</Link>
                 </div>
+                <div className="text-white font-bold" />
+
             </div>
         </nav>
+
+
 
 
 
